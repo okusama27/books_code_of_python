0c5b315a2a33ed2ebcaa3cc60dec471659ee6d63
okusama27 <kaz@iron-hot.com>
2018-02-04 23:22:46 +0900

    Delete code

--- effective_python/56/utils.py
+++
@@ -1,9 +0,0 @@
-def to_str(data):
-    if isinstance(data, str):
-        return data
-    elif isinstance(data, bytes):
-        reutrn data.decode('utf-8')
-    else:
-        raise TypeError('Must supply str or bytes,'
-                        'found: %r' % data)
-
